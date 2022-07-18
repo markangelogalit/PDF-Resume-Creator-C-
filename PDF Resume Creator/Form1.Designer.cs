8b16d592ad25451d269d81ad45a7e77033ea6a5e
Mark Angelo Galit <rankm12345@gmail.com>
2022-07-19 01:55:36 +0800

    complete code for pdf

--- PDF Resume Creator/Form1.Designer.cs
+++ PDF Resume Creator/Form1.Designer.cs
@@ -54,27 +54,27 @@
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(297, 485);
+            this.btn1.Location = new System.Drawing.Point(372, 396);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(75, 23);
             this.btn1.TabIndex = 0;
             this.btn1.Text = "GENERATE";
             this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(268, 9);
+            this.lbl1.Location = new System.Drawing.Point(344, 9);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(127, 15);
+            this.lbl1.Size = new System.Drawing.Size(51, 15);
             this.lbl1.TabIndex = 1;
-            this.lbl1.Text = "PDF RESUME CREATOR";
+            this.lbl1.Text = "RESUME";
             // 
             // lbl2
             // 
             this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(12, 41);
+            this.lbl2.Location = new System.Drawing.Point(37, 52);
             this.lbl2.Name = "lbl2";
             this.lbl2.Size = new System.Drawing.Size(66, 15);
             this.lbl2.TabIndex = 2;
@@ -83,36 +83,34 @@
             // lbl3
             // 
             this.lbl3.AutoSize = true;
-            this.lbl3.Location = new System.Drawing.Point(484, 41);
+            this.lbl3.Location = new System.Drawing.Point(640, 52);
             this.lbl3.Name = "lbl3";
             this.lbl3.Size = new System.Drawing.Size(41, 15);
             this.lbl3.TabIndex = 3;
             this.lbl3.Text = "SKILLS";
-            this.lbl3.Click += new System.EventHandler(this.label3_Click);
             // 
             // lbl4
             // 
             this.lbl4.AutoSize = true;
-            this.lbl4.Location = new System.Drawing.Point(23, 288);
+            this.lbl4.Location = new System.Drawing.Point(43, 269);
             this.lbl4.Name = "lbl4";
-            this.lbl4.Size = new System.Drawing.Size(109, 15);
+            this.lbl4.Size = new System.Drawing.Size(72, 15);
             this.lbl4.TabIndex = 4;
-            this.lbl4.Text = "WORK EXPERIENCE";
+            this.lbl4.Text = "EXPERIENCE";
             // 
             // lbl5
             // 
             this.lbl5.AutoSize = true;
-            this.lbl5.Location = new System.Drawing.Point(497, 288);
+            this.lbl5.Location = new System.Drawing.Point(646, 242);
             this.lbl5.Name = "lbl5";
             this.lbl5.Size = new System.Drawing.Size(71, 15);
             this.lbl5.TabIndex = 5;
             this.lbl5.Text = "EDUCATION";
-            this.lbl5.Click += new System.EventHandler(this.label5_Click);
             // 
             // lbl6
             // 
             this.lbl6.AutoSize = true;
-            this.lbl6.Location = new System.Drawing.Point(12, 75);
+            this.lbl6.Location = new System.Drawing.Point(37, 78);
             this.lbl6.Name = "lbl6";
             this.lbl6.Size = new System.Drawing.Size(64, 15);
             this.lbl6.TabIndex = 6;
@@ -121,17 +119,16 @@
             // lbl7
             // 
             this.lbl7.AutoSize = true;
-            this.lbl7.Location = new System.Drawing.Point(12, 102);
+            this.lbl7.Location = new System.Drawing.Point(37, 105);
             this.lbl7.Name = "lbl7";
             this.lbl7.Size = new System.Drawing.Size(79, 15);
             this.lbl7.TabIndex = 7;
             this.lbl7.Text = "MiddleName:";
-            this.lbl7.Click += new System.EventHandler(this.lbl7_Click);
             // 
             // lbl8
             // 
             this.lbl8.AutoSize = true;
-            this.lbl8.Location = new System.Drawing.Point(12, 134);
+            this.lbl8.Location = new System.Drawing.Point(37, 129);
             this.lbl8.Name = "lbl8";
             this.lbl8.Size = new System.Drawing.Size(57, 15);
             this.lbl8.TabIndex = 8;
@@ -140,7 +137,7 @@
             // lbl9
             // 
             this.lbl9.AutoSize = true;
-            this.lbl9.Location = new System.Drawing.Point(12, 163);
+            this.lbl9.Location = new System.Drawing.Point(37, 156);
             this.lbl9.Name = "lbl9";
             this.lbl9.Size = new System.Drawing.Size(52, 15);
             this.lbl9.TabIndex = 9;
@@ -149,7 +146,7 @@
             // lbl10
             // 
             this.lbl10.AutoSize = true;
-            this.lbl10.Location = new System.Drawing.Point(12, 188);
+            this.lbl10.Location = new System.Drawing.Point(37, 185);
             this.lbl10.Name = "lbl10";
             this.lbl10.Size = new System.Drawing.Size(96, 15);
             this.lbl10.TabIndex = 10;
@@ -158,7 +155,7 @@
             // lbl11
             // 
             this.lbl11.AutoSize = true;
-            this.lbl11.Location = new System.Drawing.Point(12, 218);
+            this.lbl11.Location = new System.Drawing.Point(37, 216);
             this.lbl11.Name = "lbl11";
             this.lbl11.Size = new System.Drawing.Size(39, 15);
             this.lbl11.TabIndex = 11;
@@ -167,7 +164,7 @@
             // lbl12
             // 
             this.lbl12.AutoSize = true;
-            this.lbl12.Location = new System.Drawing.Point(484, 75);
+            this.lbl12.Location = new System.Drawing.Point(640, 89);
             this.lbl12.Name = "lbl12";
             this.lbl12.Size = new System.Drawing.Size(53, 15);
             this.lbl12.TabIndex = 12;
@@ -176,37 +173,34 @@
             // lbl13
             // 
             this.lbl13.AutoSize = true;
-            this.lbl13.Location = new System.Drawing.Point(484, 102);
+            this.lbl13.Location = new System.Drawing.Point(640, 117);
             this.lbl13.Name = "lbl13";
             this.lbl13.Size = new System.Drawing.Size(70, 15);
             this.lbl13.TabIndex = 13;
             this.lbl13.Text = "SecondSkill:";
-            this.lbl13.Click += new System.EventHandler(this.label13_Click);
             // 
             // lbl14
             // 
             this.lbl14.AutoSize = true;
-            this.lbl14.Location = new System.Drawing.Point(484, 134);
+            this.lbl14.Location = new System.Drawing.Point(640, 143);
             this.lbl14.Name = "lbl14";
             this.lbl14.Size = new System.Drawing.Size(58, 15);
             this.lbl14.TabIndex = 14;
             this.lbl14.Text = "ThirdSkill:";
-            this.lbl14.Click += new System.EventHandler(this.lbl14_Click);
             // 
             // lbl15
             // 
             this.lbl15.AutoSize = true;
-            this.lbl15.Location = new System.Drawing.Point(484, 163);
+            this.lbl15.Location = new System.Drawing.Point(640, 173);
             this.lbl15.Name = "lbl15";
             this.lbl15.Size = new System.Drawing.Size(66, 15);
             this.lbl15.TabIndex = 15;
             this.lbl15.Text = "FourthSkill:";
-            this.lbl15.Click += new System.EventHandler(this.lbl15_Click);
             // 
             // lbl16
             // 
             this.lbl16.AutoSize = true;
-            this.lbl16.Location = new System.Drawing.Point(484, 197);
+            this.lbl16.Location = new System.Drawing.Point(640, 200);
             this.lbl16.Name = "lbl16";
             this.lbl16.Size = new System.Drawing.Size(55, 15);
             this.lbl16.TabIndex = 16;
@@ -215,16 +209,16 @@
             // lbl17
             // 
             this.lbl17.AutoSize = true;
-            this.lbl17.Location = new System.Drawing.Point(23, 329);
+            this.lbl17.Location = new System.Drawing.Point(43, 307);
             this.lbl17.Name = "lbl17";
-            this.lbl17.Size = new System.Drawing.Size(67, 15);
+            this.lbl17.Size = new System.Drawing.Size(95, 15);
             this.lbl17.TabIndex = 17;
-            this.lbl17.Text = "Experience:";
+            this.lbl17.Text = "WorkExperience:";
             // 
             // lbl18
             // 
             this.lbl18.AutoSize = true;
-            this.lbl18.Location = new System.Drawing.Point(497, 313);
+            this.lbl18.Location = new System.Drawing.Point(646, 269);
             this.lbl18.Name = "lbl18";
             this.lbl18.Size = new System.Drawing.Size(48, 15);
             this.lbl18.TabIndex = 18;
@@ -233,7 +227,7 @@
             // lbl19
             // 
             this.lbl19.AutoSize = true;
-            this.lbl19.Location = new System.Drawing.Point(497, 339);
+            this.lbl19.Location = new System.Drawing.Point(646, 293);
             this.lbl19.Name = "lbl19";
             this.lbl19.Size = new System.Drawing.Size(97, 15);
             this.lbl19.TabIndex = 19;
@@ -242,17 +236,16 @@
             // lbl20
             // 
             this.lbl20.AutoSize = true;
-            this.lbl20.Location = new System.Drawing.Point(497, 363);
+            this.lbl20.Location = new System.Drawing.Point(646, 319);
             this.lbl20.Name = "lbl20";
             this.lbl20.Size = new System.Drawing.Size(65, 15);
             this.lbl20.TabIndex = 20;
             this.lbl20.Text = "Secondary:";
-            this.lbl20.Click += new System.EventHandler(this.label20_Click);
             // 
             // lbl21
             // 
             this.lbl21.AutoSize = true;
-            this.lbl21.Location = new System.Drawing.Point(497, 390);
+            this.lbl21.Location = new System.Drawing.Point(646, 346);
             this.lbl21.Name = "lbl21";
             this.lbl21.Size = new System.Drawing.Size(51, 15);
             this.lbl21.TabIndex = 21;
@@ -262,7 +255,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 520);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lbl21);
             this.Controls.Add(this.lbl20);
             this.Controls.Add(this.lbl19);
@@ -287,7 +280,6 @@
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -43,9 +43,10 @@ namespace PDF_Resume_Creator
             public string Secondary { get; set; }
             public string Primary { get; set; }
 
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn1_Click(object sender, EventArgs e)
         {
             string jsonFromFile;
             using (var reader = new StreamReader(_fileName))
@@ -74,67 +75,79 @@ namespace PDF_Resume_Creator
             string Secondary = resumeFromJson.Secondary;
             string Primary = resumeFromJson.Primary;
 
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog()) ;
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.InitialDirectory = @"C:\Users\admin\Documents\RESUME PDF";
+                saveFileDialog.FileName = Surname + " " + FirstName + ".pdf";
+                saveFileDialog.Filter = "PDF|*.pdf";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    PdfDocument pdf = new PdfDocument();
+                    pdf.Info.Title = Surname + "_" + "Resume";
+                    PdfPage page = pdf.AddPage();
 
 
-        }
+                    XGraphics graph = XGraphics.FromPdfPage(page);
+                    XFont bigfont = new XFont("Arial", 20, XFontStyle.Bold);
+                    XFont smallfont = new XFont("Arial", 16, XFontStyle.Regular);
+                    XFont titlefont = new XFont("Arial", 37, XFontStyle.Bold);
+                    XPen pen = new XPen(XColors.Aquamarine, 20);
+                    XPen linerleft = new XPen(XColors.Black, 1); ;
+                    XPen linerright = new XPen(XColors.Black, 1);
 
 
+                    graph.DrawRoundedRectangle(XBrushes.Cornsilk, 0, 0, page.Width.Point, page.Height.Point, 30, 20);
+                    graph.DrawRoundedRectangle(XBrushes.Aquamarine, 200, 50, page.Width.Point, page.Height.Point, 100, 100);
+                    graph.DrawRectangle(pen, 0, 0, page.Width.Point, page.Height.Point);
+                    graph.DrawString("COMPUTER ENGINEERING STUDENT", bigfont, XBrushes.Black, new XRect(0, 20, page.Width.Point - 20, page.Height.Point - 50), XStringFormats.TopRight);
+                    int marginleft = 25;
+                    int initialleft = 200;
+                    string jpg = @"C:\Users\admin\Documents\MARK FOLDER FOR GRADE 12\Mark Angelo Galit.jpg";
+                    XImage image = XImage.FromFile(jpg);
+                    graph.DrawImage(image, marginleft, 50, 150, 150);
 
 
+                    graph.DrawString("CONTACTS:", bigfont, XBrushes.Black, new XRect(marginleft, initialleft + 65, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(FirstName, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(MiddleName, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(Surname, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(Address, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(ContactNumber, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+                    graph.DrawString(Email, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.TopLeft);
+
+                    int marginright = 220;
+                    int initialright = 200;
+                    int TopRight = 200;
 
 
+                    graph.DrawString("SKILLS:", bigfont, XBrushes.Black, new XRect(marginright, initialright + 65, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+                    graph.DrawString(FirstSkill, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+                    graph.DrawString(SecondSkill, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+                    graph.DrawString(ThirdSkill, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+                    graph.DrawString(FourthSkill, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+                    graph.DrawString(FifthSkill, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.TopRight);
+
+                    int BottomLeft = 200;
+                    int BottomRight = 200;
+
+                    graph.DrawString("EXPERIENCE:", bigfont, XBrushes.Black, new XRect(marginleft, initialleft + 65, page.Width.Point, page.Height.Point), XStringFormats.BottomLeft);
+                    graph.DrawString(WorkExperience, smallfont, XBrushes.Black, new XRect(marginleft, initialleft + 110, page.Width.Point, page.Height.Point), XStringFormats.BottomLeft);
+
+                    graph.DrawString("EDUCATION:", bigfont, XBrushes.Black, new XRect(marginright, initialright + 65, page.Width.Point, page.Height.Point), XStringFormats.BottomRight);
+                    graph.DrawString(Tertiary, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.BottomRight);
+                    graph.DrawString(UpperSecondary, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.BottomRight);
+                    graph.DrawString(Secondary, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.BottomRight);
+                    graph.DrawString(Primary, smallfont, XBrushes.Black, new XRect(marginright, initialright + 110, page.Width.Point, page.Height.Point), XStringFormats.BottomRight);
 
 
+                    pdf.Save(saveFileDialog.FileName);
+                }
+                MessageBox.Show("Thank you for using this program.");
 
+            }
+            Application.Restart();
+            Environment.Exit(0);
 
-
-
-
-
-
-
-
-
-
-
-        private void label20_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label13_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl14_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void lbl15_Click(object sender, EventArgs e)
-        {
 
         }
     }
