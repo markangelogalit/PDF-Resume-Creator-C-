ed0f3027fbf930ae738818527a2a09279a3efb17
Mark Angelo Galit <rankm12345@gmail.com>
2022-07-18 22:51:57 +0800

    adding label and renaming it

--- PDF Resume Creator/Form1.Designer.cs
+++ PDF Resume Creator/Form1.Designer.cs
@@ -29,32 +29,290 @@
         private void InitializeComponent()
         {
             this.btn1 = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.lbl13 = new System.Windows.Forms.Label();
+            this.lbl14 = new System.Windows.Forms.Label();
+            this.lbl15 = new System.Windows.Forms.Label();
+            this.lbl16 = new System.Windows.Forms.Label();
+            this.lbl17 = new System.Windows.Forms.Label();
+            this.lbl18 = new System.Windows.Forms.Label();
+            this.lbl19 = new System.Windows.Forms.Label();
+            this.lbl20 = new System.Windows.Forms.Label();
+            this.lbl21 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(74, 129);
+            this.btn1.Location = new System.Drawing.Point(297, 485);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(75, 23);
             this.btn1.TabIndex = 0;
-            this.btn1.Text = "CREATE";
+            this.btn1.Text = "GENERATE";
             this.btn1.UseVisualStyleBackColor = true;
             this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(268, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(38, 15);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(12, 41);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(38, 15);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "label2";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(484, 41);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(38, 15);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "label3";
+            this.lbl3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(29, 288);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(38, 15);
+            this.lbl4.TabIndex = 4;
+            this.lbl4.Text = "label4";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Location = new System.Drawing.Point(497, 288);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(38, 15);
+            this.lbl5.TabIndex = 5;
+            this.lbl5.Text = "label5";
+            this.lbl5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(12, 75);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(38, 15);
+            this.lbl6.TabIndex = 6;
+            this.lbl6.Text = "label6";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(12, 102);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(38, 15);
+            this.lbl7.TabIndex = 7;
+            this.lbl7.Text = "label7";
+            // 
+            // lbl8
+            // 
+            this.lbl8.AutoSize = true;
+            this.lbl8.Location = new System.Drawing.Point(12, 134);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(38, 15);
+            this.lbl8.TabIndex = 8;
+            this.lbl8.Text = "label8";
+            // 
+            // lbl9
+            // 
+            this.lbl9.AutoSize = true;
+            this.lbl9.Location = new System.Drawing.Point(12, 163);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(38, 15);
+            this.lbl9.TabIndex = 9;
+            this.lbl9.Text = "label9";
+            // 
+            // lbl10
+            // 
+            this.lbl10.AutoSize = true;
+            this.lbl10.Location = new System.Drawing.Point(12, 188);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(44, 15);
+            this.lbl10.TabIndex = 10;
+            this.lbl10.Text = "label10";
+            // 
+            // lbl11
+            // 
+            this.lbl11.AutoSize = true;
+            this.lbl11.Location = new System.Drawing.Point(12, 218);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(44, 15);
+            this.lbl11.TabIndex = 11;
+            this.lbl11.Text = "label11";
+            // 
+            // lbl12
+            // 
+            this.lbl12.AutoSize = true;
+            this.lbl12.Location = new System.Drawing.Point(484, 75);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(44, 15);
+            this.lbl12.TabIndex = 12;
+            this.lbl12.Text = "label12";
+            // 
+            // lbl13
+            // 
+            this.lbl13.AutoSize = true;
+            this.lbl13.Location = new System.Drawing.Point(484, 102);
+            this.lbl13.Name = "lbl13";
+            this.lbl13.Size = new System.Drawing.Size(32, 15);
+            this.lbl13.TabIndex = 13;
+            this.lbl13.Text = "lbl13";
+            this.lbl13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // lbl14
+            // 
+            this.lbl14.AutoSize = true;
+            this.lbl14.Location = new System.Drawing.Point(484, 134);
+            this.lbl14.Name = "lbl14";
+            this.lbl14.Size = new System.Drawing.Size(44, 15);
+            this.lbl14.TabIndex = 14;
+            this.lbl14.Text = "label14";
+            // 
+            // lbl15
+            // 
+            this.lbl15.AutoSize = true;
+            this.lbl15.Location = new System.Drawing.Point(484, 163);
+            this.lbl15.Name = "lbl15";
+            this.lbl15.Size = new System.Drawing.Size(44, 15);
+            this.lbl15.TabIndex = 15;
+            this.lbl15.Text = "label15";
+            // 
+            // lbl16
+            // 
+            this.lbl16.AutoSize = true;
+            this.lbl16.Location = new System.Drawing.Point(484, 197);
+            this.lbl16.Name = "lbl16";
+            this.lbl16.Size = new System.Drawing.Size(44, 15);
+            this.lbl16.TabIndex = 16;
+            this.lbl16.Text = "label16";
+            // 
+            // lbl17
+            // 
+            this.lbl17.AutoSize = true;
+            this.lbl17.Location = new System.Drawing.Point(23, 329);
+            this.lbl17.Name = "lbl17";
+            this.lbl17.Size = new System.Drawing.Size(44, 15);
+            this.lbl17.TabIndex = 17;
+            this.lbl17.Text = "label17";
+            // 
+            // lbl18
+            // 
+            this.lbl18.AutoSize = true;
+            this.lbl18.Location = new System.Drawing.Point(497, 313);
+            this.lbl18.Name = "lbl18";
+            this.lbl18.Size = new System.Drawing.Size(44, 15);
+            this.lbl18.TabIndex = 18;
+            this.lbl18.Text = "label18";
+            // 
+            // lbl19
+            // 
+            this.lbl19.AutoSize = true;
+            this.lbl19.Location = new System.Drawing.Point(497, 339);
+            this.lbl19.Name = "lbl19";
+            this.lbl19.Size = new System.Drawing.Size(44, 15);
+            this.lbl19.TabIndex = 19;
+            this.lbl19.Text = "label19";
+            // 
+            // lbl20
+            // 
+            this.lbl20.AutoSize = true;
+            this.lbl20.Location = new System.Drawing.Point(497, 363);
+            this.lbl20.Name = "lbl20";
+            this.lbl20.Size = new System.Drawing.Size(44, 15);
+            this.lbl20.TabIndex = 20;
+            this.lbl20.Text = "label20";
+            this.lbl20.Click += new System.EventHandler(this.label20_Click);
+            // 
+            // lbl21
+            // 
+            this.lbl21.AutoSize = true;
+            this.lbl21.Location = new System.Drawing.Point(497, 390);
+            this.lbl21.Name = "lbl21";
+            this.lbl21.Size = new System.Drawing.Size(44, 15);
+            this.lbl21.TabIndex = 21;
+            this.lbl21.Text = "label21";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(251, 323);
+            this.ClientSize = new System.Drawing.Size(684, 520);
+            this.Controls.Add(this.lbl21);
+            this.Controls.Add(this.lbl20);
+            this.Controls.Add(this.lbl19);
+            this.Controls.Add(this.lbl18);
+            this.Controls.Add(this.lbl17);
+            this.Controls.Add(this.lbl16);
+            this.Controls.Add(this.lbl15);
+            this.Controls.Add(this.lbl14);
+            this.Controls.Add(this.lbl13);
+            this.Controls.Add(this.lbl12);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
             this.Controls.Add(this.btn1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button btn1;
+        private Label lbl1;
+        private Label lbl2;
+        private Label lbl3;
+        private Label lbl4;
+        private Label lbl5;
+        private Label lbl6;
+        private Label lbl7;
+        private Label lbl8;
+        private Label lbl9;
+        private Label lbl10;
+        private Label lbl11;
+        private Label lbl12;
+        private Label lbl13;
+        private Label lbl14;
+        private Label lbl15;
+        private Label lbl16;
+        private Label lbl17;
+        private Label lbl18;
+        private Label lbl19;
+        private Label lbl20;
+        private Label lbl21;
     }
 }
--- PDF Resume Creator/Form1.cs
+++ PDF Resume Creator/Form1.cs
@@ -30,12 +30,47 @@ namespace PDF_Resume_Creator
             public string MiddleName { get; set; }
             public string Surname { get; set; }
             public string Address { get; set; }
-
-
+            public string ContactNumber { get; set; }
+            public string Email { get; set; }
+            public string FirstSkill { get; set; }
+            public string SecondSkill { get; set; }
+            public string ThirdSkill { get; set; }
+            public string FourthSkill { get; set; }
+            public string FifthSkill { get; set; }
+            public string WorkExperience { get; set; }
+            public string Tertiary { get; set; }
+            public string UpperSecondary { get; set; }
+            public string Secondary { get; set; }
+            public string Primary { get; set; }
 
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
         {
 
         }
